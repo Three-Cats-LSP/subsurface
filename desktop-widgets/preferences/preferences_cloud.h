651b5dbbe49924357c6f3889af44c78dbf05c575
Roman Markovtsev <sns@rvm.cc>
2026-08-11 02:19:49 +0900

    cloud: add desktop backup controls

--- desktop-widgets/preferences/preferences_cloud.h
+++ desktop-widgets/preferences/preferences_cloud.h
@@ -36,6 +36,7 @@ private:
 	CloudSyncManager *neoCloudSync = nullptr;
 	QHash<QString, QLabel *> neoStatusLabels;
 	QHash<QString, QPushButton *> neoActionButtons;
+	QHash<QString, QPushButton *> neoBackupButtons;
 };
 
 #endif
