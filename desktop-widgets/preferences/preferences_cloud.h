6c3ca184eccec96a5aed9a4367fd8c5d65b87e77
Roman Markovtsev <sns@rvm.cc>
2026-08-11 02:12:00 +0900

    cloud: prepare desktop Neo provider controls

--- desktop-widgets/preferences/preferences_cloud.h
+++ desktop-widgets/preferences/preferences_cloud.h
@@ -4,9 +4,15 @@
 
 #include "abstractpreferenceswidget.h"
 
+#include <QHash>
+
 namespace Ui {
 	class PreferencesCloud;
 }
+
+class CloudSyncManager;
+class QLabel;
+class QPushButton;
 
 class PreferencesCloud : public AbstractPreferencesWidget {
 	Q_OBJECT
@@ -23,7 +29,13 @@ public slots:
 	void on_resetPassword_clicked();
 
 private:
+	void setupNeoCloudProviders();
+	void updateNeoCloudProviders();
+
 	Ui::PreferencesCloud *ui;
+	CloudSyncManager *neoCloudSync = nullptr;
+	QHash<QString, QLabel *> neoStatusLabels;
+	QHash<QString, QPushButton *> neoActionButtons;
 };
 
 #endif
