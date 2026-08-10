f1b5ab7d44cf51f138deea4a5472ea236607eb34
Roman Markovtsev <sns@rvm.cc>
2026-08-11 01:49:29 +0900

    cloud: route Android OAuth deep links

--- android-mobile/src/org/subsurfacedivelog/mobile/SubsurfaceMobileActivity.java
+++ android-mobile/src/org/subsurfacedivelog/mobile/SubsurfaceMobileActivity.java
@@ -1,11 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0
 
-// Java classes for handling Intents on Android (which in this case means
-// Subsurface-mobile receiving notifaction that an FTDI dive computer was
-// plugged in
-
-// Created while looking at https://github.com/ekke/ekkesSHAREexample
-
 package org.subsurfacedivelog.mobile;
 
 import org.qtproject.qt.android.bindings.QtActivity;
@@ -22,32 +16,20 @@ import java.io.File;
 import android.net.Uri;
 import androidx.core.content.FileProvider;
 import androidx.core.app.ShareCompat;
-import android.content.pm.PackageManager;
-import java.util.List;
-import android.content.pm.ResolveInfo;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
-// this is the main class that will be run at start
 public class SubsurfaceMobileActivity extends QtActivity
 {
-	// FileProvider declares an 'authority' in AndroidManifest.xml
 	private static String fileProviderAuthority="org.subsurfacedivelog.mobile.fileprovider";
 
-	// you can share one or two files
 	public boolean shareViaEmail(String subject, String recipient, String body, String path1, String path2) {
 		Log.d(TAG + " shareFile - trying to share: ", path1 + " and " + path2 + " to " + recipient);
-
-		// Can't get this to work building my own intent, so let's use the IntentBuilder
 		Intent shareFileIntent = new ShareCompat.IntentBuilder(this).getIntent();
 		shareFileIntent.setAction(Intent.ACTION_SEND_MULTIPLE);
-		// recipients are always an array, even if there's only one
 		shareFileIntent.putExtra(Intent.EXTRA_EMAIL, new String[] { recipient });
 		shareFileIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
 		shareFileIntent.putExtra(Intent.EXTRA_TEXT, body);
 
-		// now figure out the URI we need to share the first file
 		File fileToShare = new File(path1);
 		Uri uri;
 		try {
@@ -56,14 +38,9 @@ public class SubsurfaceMobileActivity extends QtActivity
 			Log.d(TAG + " shareFile - cannot get URI for ", path1);
 			return false;
 		}
-		Log.d(TAG + " shareFile - URI for file: ", uri.toString());
 
-		// because we allow up to two attachments, we use ACTION_SEND_MULTIPLE and so the attachments
-		// need to be an ArrayList - even if we only add one attachment this still works
 		ArrayList<Uri> attachments = new ArrayList<Uri>();
 		attachments.add(uri);
-
-		// if there is a second file name (that's for support emails) add it and set this up as support email as well
 		if (!path2.isEmpty()) {
 			fileToShare = new File(path2);
 			try {
@@ -72,16 +49,12 @@ public class SubsurfaceMobileActivity extends QtActivity
 				Log.d(TAG + " shareFile - cannot get URI for ", path2);
 				return false;
 			}
-			Log.d(TAG + " shareFile - URI for file: ", uri.toString());
 			attachments.add(uri);
 		}
 		shareFileIntent.setType("text/plain");
 		shareFileIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, attachments);
 		shareFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 		shareFileIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-		Log.d(TAG + " sendFile ", " create activity for the Intent");
-		// this actually allows sharing with any app that will take "text/plain" files, including Dropbox, etc
-		// in order for the recipient / subject to work, the user needs to be clever enough to share with an email app
 		this.startActivity(shareFileIntent);
 		return true;
 	}
@@ -95,97 +68,94 @@ public class SubsurfaceMobileActivity extends QtActivity
 
 	public static boolean isIntentPending;
 	public static boolean isInitialized;
+	private static String pendingOAuthUrl;
 	private static final String TAG = "subsurfacedivelog.mobile";
 	public static native void setUsbDevice(UsbDevice usbDevice);
 	public static native void restartDownload(UsbDevice usbDevice);
+	public static native void oauthCallback(String url);
 	private static Context appContext;
 
-	// we need to provide two endpoints:
-	// onNewIntent if we receive an Intent while running
-	// onCreate    if we were started by an Intent
+	private static boolean isOAuthIntent(Intent intent)
+	{
+		Uri data = intent == null ? null : intent.getData();
+		return data != null && "subsurface-neo".equals(data.getScheme()) && "oauth".equals(data.getHost());
+	}
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		Log.i(TAG + " onCreate", "onCreate SubsurfaceMobileActivity");
 		super.onCreate(savedInstanceState);
-
-		// Ensure app content does not draw behind system bars.
-		// The theme sets windowOptOutEdgeToEdgeEnforcement for Android 15+;
-		// this call handles pre-Android 15 devices.
 		androidx.core.view.WindowCompat.setDecorFitsSystemWindows(getWindow(), true);
-
 		appContext = getApplicationContext();
 
-		// now we're checking if the App was started from another Android App via Intent
 		Intent theIntent = getIntent();
-		if (theIntent != null) {
-			String theAction = theIntent.getAction();
-			if (theAction != null) {
-				Log.i(TAG + " onCreate", theAction);
-				isIntentPending = true;
-			}
+		if (isOAuthIntent(theIntent)) {
+			pendingOAuthUrl = theIntent.getDataString();
+			Log.i(TAG + " onCreate", "OAuth callback pending");
+		} else if (theIntent != null && theIntent.getAction() != null) {
+			Log.i(TAG + " onCreate", theIntent.getAction());
+			isIntentPending = true;
 		}
 
-		// Register the usb permission intent filter.
 		IntentFilter filter = new IntentFilter("org.subsurfacedivelog.mobile.USB_PERMISSION");
 		registerReceiver(usbReceiver, filter, Context.RECEIVER_NOT_EXPORTED);
+	}
 
-	} // onCreate
-
-	// if we are opened from other apps:
 	@Override
 	public void onNewIntent(Intent intent)
 	{
-		Log.i(TAG + " onNewIntent", intent.getAction());
+		Log.i(TAG + " onNewIntent", String.valueOf(intent.getAction()));
+		super.onNewIntent(intent);
+		setIntent(intent);
+
+		if (isOAuthIntent(intent)) {
+			String callback = intent.getDataString();
+			if (isInitialized) {
+				oauthCallback(callback);
+			} else {
+				pendingOAuthUrl = callback;
+			}
+			return;
+		}
+
 		UsbDevice device = getUsbDevice(intent);
 		if (device == null) {
 			Log.i(TAG + " onNewIntent", "null device");
 			return;
 		}
-		Log.i(TAG + " onNewIntent toString", device.toString());
-		super.onNewIntent(intent);
-		setIntent(intent);
-		// Intent will be processed, if all is initialized and Qt / QML can handle the event
-		if (isInitialized) {
+		if (isInitialized)
 			processIntent();
-		} else {
+		else
 			isIntentPending = true;
-		}
-	} // onNewIntent
+	}
 
 	public void checkPendingIntents()
 	{
 		isInitialized = true;
+		if (pendingOAuthUrl != null) {
+			String callback = pendingOAuthUrl;
+			pendingOAuthUrl = null;
+			Log.i(TAG + " checkPendingIntents", "processing OAuth callback");
+			oauthCallback(callback);
+		}
 		if (isIntentPending) {
 			isIntentPending = false;
-			Log.i(TAG + " checkPendingIntents", "checkPendingIntents: true");
 			processIntent();
-		} else {
-			Log.i(TAG + " checkPendingIntents", "nothingPending");
 		}
-	} // checkPendingIntents
-
+	}
 
 	private void processIntent()
 	{
 		Intent intent = getIntent();
-		if (intent == null) {
-			Log.i(TAG + " processIntent", "intent is null");
+		if (intent == null || isOAuthIntent(intent))
 			return;
-		}
-		Log.i(TAG + " processIntent", intent.getAction());
 		UsbDevice device = getUsbDevice(intent);
-		if (device == null) {
-			Log.i(TAG + " processIntent", "null device");
+		if (device == null)
 			return;
-		}
-		Log.i(TAG + " processIntent device name", device.getDeviceName());
 		try {
 			setUsbDevice(device);
 		} catch (LinkageError e) {
-			// The native library may not be fully loaded yet.
-			// checkPendingIntents() is only called once
-			// during startup, so schedule a one-shot retry
 			Log.w(TAG + " processIntent", "native not ready, scheduling retry", e);
 			isIntentPending = true;
 			new Handler(Looper.getMainLooper()).postDelayed(() -> {
@@ -202,8 +172,7 @@ public class SubsurfaceMobileActivity extends QtActivity
 				}
 			}, 500);
 		}
-	} // processIntent
-
+	}
 
 	private final BroadcastReceiver usbReceiver = new BroadcastReceiver()
 	{
@@ -213,11 +182,8 @@ public class SubsurfaceMobileActivity extends QtActivity
 				synchronized (this) {
 					if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
 						UsbDevice device = getUsbDevice(intent);
-						if (device == null) {
-							Log.i(TAG, " permission granted but null device");
+						if (device == null)
 							return;
-						}
-						Log.d(TAG, "USB device permission granted for " + device.getDeviceName());
 						restartDownload(device);
 					} else {
 						Log.d(TAG, "USB device permission denied");
@@ -232,8 +198,6 @@ public class SubsurfaceMobileActivity extends QtActivity
 		return appContext;
 	}
 
-	// getParcelableExtra(String) was deprecated in API 33; the typed overload
-	// requires API 33, so guard by version.
 	@SuppressWarnings("deprecation")
 	private static UsbDevice getUsbDevice(Intent intent) {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
