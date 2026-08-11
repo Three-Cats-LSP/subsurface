a30fbde4abc363fc2774a9e2f5e170228e057782
Roman Markovtsev <sns@rvm.cc>
2026-08-11 19:42:18 +0900

    mobile: restore swipe model declaration

--- qt-models/mobilelistmodel.h
+++ qt-models/mobilelistmodel.h
@@ -158,7 +158,7 @@ private:
 	// Update elements
 	void initData();
 	int removeTopLevel(int begin, int end);
-	int addTopLevel(int row, std::vector<int> items);
+	void addTopLevel(int row, std::vector<int> items);
 	void updateTopLevel(int row, int delta);
 signals:
 	void currentDiveChanged(QModelIndex index);
