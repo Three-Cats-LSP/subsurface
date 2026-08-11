a5b36f91fd8af37387a9e101967aef1638a5fd27
Roman Markovtsev <sns@rvm.cc>
2026-08-11 19:41:01 +0900

    mobile: expose dive mode role

--- qt-models/mobilelistmodel.h
+++ qt-models/mobilelistmodel.h
@@ -62,7 +62,8 @@ public:
 		TripBelow,
 		TripLocationRole,
 		TripNotesRole,
-		IsInvalidRole
+		IsInvalidRole,
+		DiveModeRole
 	};
 	QHash<int, QByteArray> roleNames() const override;
 protected:
@@ -103,7 +104,7 @@ private:
 	QModelIndex mapFromSource(const QModelIndex &idx) const;
 	QModelIndex mapToSource(const QModelIndex &idx) const;
 	static void updateRowAfterRemove(const IndexRange &range, int &row);
-	static void updateRowAfterMove(const IndexRange &range, const IndexRange &dest, int &row);
+	static void updateRowAfterMove(const IndexRange &range, const IndexRange &dest, int destRow);
 	QVariant data(const QModelIndex &index, int role) const override;
 	int rowCount(const QModelIndex &parent) const override;
 	int shown() const;
